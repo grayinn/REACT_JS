61c7720975fdd4f1010f8577d8756c2afd655663
grayinn <nguyenhongnhung4155@gmail.com>
2023-06-29 17:26:43 +0700

    Update 29th6

---
+++ 3. JSX, Components, Props/3_react.js
@@ -0,0 +1,56 @@
+
+
+/**
+ * --------------------------------------- JSX ------------------------------
+ *  Javascript XML
+ *  Bable: chuyển đỏi cú pháp JSX -> Javascript
+ *  1. Tạo biến, gán JSX cho biến
+ *  2. Khi làm JSX mà muốn viết xen lẫn code JS -> sử dụng {biến js ở đây}
+ */
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+
+/**
+ * ------------------------------- 2. Components ----------------------
+ * Hooks
+ *      1. React element types: string, function / (class)
+ *          - Wrapper
+ *              - Header -> Header component
+ *              - Content -> Content component
+ *              - Footer -> Footer component
+ *      2. JSX
+ *      3. Class components and Function components
+ */
+
+
+/**
+ * ------------------------------ 3. Props ----------------------------
+ truyền dữ liệu từ (parent component) sang (child component)    
+ * 
+ *      1. Props là các đối số được truyền vào React components.
+ *      2. Props được chuyển đến các components thông qua HTML attributes.
+ *      3. Là Obj chứa thuộc tính để mô tả cho React elements mà cta tạo ra
+ * Props
+ *      - React elements (thẻ js like html)
+ *          + Sử dụng props như attribute thẻ HTML
+ *          + props class, for -> className, htmlFor
+ *                      <label htmlFor="email">Nhập vào email</label> 
+                        <input id="email" />    
+ *      - React components (function >hơn ele)
+ *          + Use props like đối số cho function
+ *          + Tên props (camelCase, - )
+ 
+ *      - Some notes:
+ *          - prop "key": đặc biệt
+ *          - prop -> đối số component (bất cứ KDL gì)
+ *          - use destructuring
+ */
+
+
+
+
+
+
+
