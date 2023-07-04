a5d50836efe9055d8b8690e5f44636c0d12129ca
grayinn <nguyenhongnhung4155@gmail.com>
2023-07-04 17:30:49 +0700

    Update 04th07

--- 4. Hooks/4_hooks.js
+++ 4. Hooks/4_hooks.js
@@ -187,7 +187,7 @@ function App() {
             />
 
             <input 
-                value={email}    // truyền vào value -> tạo two-way binding
+                value={email}    
                 onChange={e => setEmail(e.target.value)}
             />
 
@@ -197,6 +197,239 @@ function App() {
 }
 
 //export default App
+
+
+//------------------------------------------------------------------
+// --------------- Content.js
+function Content() {
+    return (
+        <h1>Hello World!</h1>
+    )
+}
+
+// --------------- App.js 
+// Enter Toggle -> Show content -> Hidden content
+
+import { useState } from 'react'
+//import Content from './Content'    // File Conten.js (module)
+function App() { // Component App
+    const [show, setShow] = useState(false)
+
+    return (
+        <div style={{ padding: 30 }}>
+            <button onClick={() => setShow(!show)}>Toggle</button>
+            {show && <Content />}   
+        </div>
+    )
+}
+//export default App
+
+
+/**
+ * -----------------------------------------------------------------------------
+ * ----------------------- useEffect HOOKS -------------------------------------
+ *              useEffect(<function>, <dependency>) 
+ */
+
+/**
+ *  3 trường hợp:    1. useEffect(function)          - render mãi
+ *                   2. useEffect(function, [])      - render 1 lần
+ *                   3. useEffect(function, [deps])  - render lại khi value trong [deps] thay đổi
+ */
+
+
+// ----- 1. useEffect(function) - Render liên tục k dừng
+import { useState, useEffect } from "react"
+
+function Timer() {
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCount((count) => count + 1)
+        }, 1000)
+    })
+
+    // function callback setTimeout, không có [deps] --> render lại nhiều lần
+    return <h1>I have rendered {count} times!</h1>
+}
+// export default Timer
+
+
+
+// ----- 2. useEffect(function, [])      - render 1 lần
+    useEffect(() => {
+        setTimeout(() => {
+            setCount((count) => count + 1)
+        }, 1000)
+    }, [])  // [deps] = []
+
+
+
+// ----- 3. useEffect(function, [deps])
+// -- Enter button + -> Count * 2 = Calculation
+import { useState, useEffect } from "react";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    const [cal, setCal] = useState(0);
+
+    useEffect(() => {
+        setCal(() => count * 2);
+    }, [count]); // [deps] = [count]
+
+    return ( 
+        <p>Count: {count}</p>,
+        <button onClick={() => setCount((c) => c + 1)}>+</button>,
+        <p>Calculation: {cal}</p>
+    )
+}
+
+// --------- Effect Cleanup (memory leaks)
+// -Timeouts, subscriptions, event listeners, and other effects that ko cần dùng tới nữa.
+
+useEffect(() => {
+    let timer = setTimeout(() => {
+    setCount((count) => count + 1);
+  }, 1000);
+
+  return () => clearTimeout(timer)       // function cleanup
+  }, []);                              
+
+  return <h1>I've rendered {count} times!</h1>;
+
+
+
+
+/**
+ * -----------------------------------------------------------------------------
+ * ----------------------- useLayoutEffect Hook (X) -------------------------------------
+ *              useLayoutEffect ( setup, dependencies? )
+ * 
+ *  Problem lq to UI (render UI SAU KHI cleanup, sau useLayoutEffect)    
+ */
+
+// App Enter "Run" -> Count + 1 
+function Content() {
+    const [count, setCount] = useState(0)
+
+    // If useEffect, handleRun call --> run 1 -> 2 -> 3 -> 4(chớp nháy) do
+    // hàm trong useEffect này sẽ run after handleRun + return
+    // useEffect ---> useLayoutEffect
+    useLayoutEffect(() => {
+        if (count > 3)
+            setCount(0)
+    }, [count])
+
+    const handleRun = () => {
+        setCount(count + 1)
+    }
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={handleRun}>Run</button>
+        </div>
+    )
+}
+//export default Content
+
+
+
+/**
+ * ----------------------------------------------------------------------------------
+ * -------------------------------- useRef Hook -------------------------------------
+ * 
+            It can be lưu trữ giá trị có thể thay đổi mà không a re-render when updated.
+            (Biến tham chiếu này sẽ được giữ nguyên giá trị của nó giữa các lần render của component, 
+*           mà không gây ra việc render lại component khi giá trị của biến tham chiếu thay đổi.)
+
+            useRef (initialValue) --> trả về Obj {current: }
+ */
+
+// App countdown 1 minute (start / stop button)
+import { useRef, useState } from 'react'
+//let timerID - Có thể - NHƯNG với React: mỗi file = 1 function component
+function App() {
+    const [count, setCount] = useState(60)
+            
+    const timerID = useRef()
+       
+    const handleStart = () => {
+        timerID.current = setInterval(() => {
+            setCount(prevCount => prevCount - 1)
+        }, 1000)
+        //console.log('Start -> ', timerID.current)
+    }
+            
+    const handleStop = () => {
+        clearInterval(timerID.current)
+        //console.log('Stop -> ', timerID.current)
+    }
+            
+    return (
+        <div style={{ padding: 30 }}>
+            <h1>{count}</h1>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button>
+        </div>
+    )
+}
+// export default App
+
+
+
+
+
+
+
+//------------------------------------------------------------------
+// --------------- Content.js
+// Use React.memo
+import { memo } from 'react'
+
+function Content() {
+    return (
+        <h1>Hello World!</h1>
+    )
+}
+//-----> export default memo(Content)
+/**
+ * -----------------------------------------------------------------------------
+ * ----------------------- React.memo (to useCallback HOOKS) -------------------------------------
+ *      1. memo() -> Higher Order Component (HOC)
+ *      2. useCallback()            
+ */
+import { useState } from 'react'
+import Content from './Content'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    const increase = () => {
+        setCount(count + 1)
+    }
+// Component con <Content /> sẽ bị re-render mỗi khi "Click-me" --> KH cần thiết
+    return (
+        <div style={{ padding: '10px 30px' }}>
+            <Content /> 
+            <h1>{count}</h1>
+            <button onClick={increase}>Click me!</button>
+        </div>
+    )
+}
+//export default App
+
+
+/**
+ * -----------------------------------------------------------------------------
+ * ----------------------- useContext Hook -------------------------------------
+ *              
+ */
+
+
+
+
 
 
 
